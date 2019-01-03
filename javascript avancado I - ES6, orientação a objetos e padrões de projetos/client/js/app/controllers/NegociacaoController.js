244d0da6d1a30acfdade2c97e5000c02764cc90b
Irlan Freitas <irlan.freitas@hotmail.com>
2019-01-02 22:10:08 -0300

    Melhorias no NegociacaoController.js

--- javascript avancado I - ES6, orientação a objetos e padrões de projetos/client/js/app/controllers/NegociacaoController.js
+++ javascript avancado I - ES6, orientação a objetos e padrões de projetos/client/js/app/controllers/NegociacaoController.js
@@ -19,17 +19,49 @@ class NegociacaoController {
         // Cancelando o submit que recarrega a página
         event.preventDefault()
         
+        // Poderia ter feio assim
         console.log(this._inputData.value.split("-"));
 
+        // Ou assim
         console.log(this._inputData.value.replace(/-/g, ','));
         
-        let data = this._inputData.value.split("-")
-        console.log( Number(data[1]) - 1 );
+        // let data = this._inputData.value.split("-")
+        // console.log( Number(data[1]) - 1 );
         
+        // Resolvendo a criação do elemento Date com o paradigma funcional
+        // Usando o spread operator, (reticências) (...)
+        // O spread, faz um casamento entre as posições do array com 
+        // os paramêtros do objeto.
+        // Operador de espelhamento.
+        
+        // Alguns links:
+        // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Spread_operator
+        // https://udgwebdev.com/usando-spread-operator-do-es6/
+        // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
-        
+        let data = new Date(...
+            this._inputData.value
+            .split("-")
+            // O código foi simplificado ainda mais com Arrow Functions () => {}
+            .map( (item, index) => {
+                // Se for o indice 2, o mês, subtraia um
+                // if (index == 1) {
+                //     return item - 1;   
+                // }
+                // // Se não só retorne o valor.
+                // return item;
+
+                // Explicação disso:
+                // 0 % 2 = 0, 1 % 2 = 1, 2 % 2 = 0
+                // Sempre que estiver na posição do mês irá 
+                // subtrair 1.
+
+                return item - index % 2;
+            })
+        );
+
         let negociacao = new Negociacao(
-            new Date(this._inputData.value.split("-")), 
+            data, 
             this._inputQuantidade.value, 
             this._inputValor.value)
         
