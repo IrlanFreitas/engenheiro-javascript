94251ef2cd386d26fba7af8c8a92a0cd8dd5a030
Irlan Freitas <irlan.junior@solutis.com.br>
2019-09-18 15:05:44 -0300

    Testes com os proxies

---
+++ testes/proxy/Funcionario.js
@@ -0,0 +1,14 @@
+class Funcionario {
+
+    constructor(email) {
+        this._email = email;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(email) {
+        this._email = email;
+    }
+}
